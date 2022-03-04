fb09b4e8a9bf97bf08868ec9b6eb3f4b528e439b
developerdesigner18 <developer.designer18@gmail.com>
2022-03-04 10:35:04 +0530

    updated BarChart, added google map

---
+++ src/components/BarChart.js
@@ -0,0 +1,98 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { chartData } from "../data/chartData";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: true,
+      text: "Monthly Cash Flow & Loan Balance",
+    },
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const BarChart = () => {
+  const data = {
+    labels: chartData.map((elm) => elm.month),
+    datasets: [
+      {
+        label: "Land Acquisition Cost",
+        data: chartData.map((elm) => elm.landAcquisitionCost),
+        backgroundColor: "#E7787A",
+      },
+      {
+        label: "Design Fees",
+        data: chartData.map((elm) => elm.designFees),
+        backgroundColor: "#7AB472",
+      },
+      {
+        label: "Project Contingency",
+        data: chartData.map((elm) => elm.projectContingency),
+        backgroundColor: "#DEEDDC",
+      },
+      {
+        label: "Built Form",
+        data: chartData.map((elm) => elm.builtForm),
+        backgroundColor: "#F9DEDE",
+      },
+      {
+        label: "Preliminary Works",
+        data: chartData.map((elm) => elm.preliminaryWorks),
+        backgroundColor: "#A2CECA",
+      },
+      {
+        label: "Sales Costs",
+        data: chartData.map((elm) => elm.saleCosts),
+        backgroundColor: "#F0D46D",
+      },
+      {
+        label: "Site Preparation",
+        data: chartData.map((elm) => elm.sitePreparation),
+        backgroundColor: "#91C5C1",
+      },
+      {
+        label: "Taxes and Duties",
+        data: chartData.map((elm) => elm.taxesAndDuties),
+        backgroundColor: "#D3E8E7",
+      },
+      {
+        label: "Infrastructuire Contributions",
+        data: chartData.map((elm) => elm.infrastructureContributions),
+        backgroundColor: "#C3DFDD",
+      },
+    ],
+  };
+
+  return <Bar className="my-3" options={options} data={data} />;
+};
+
+export default BarChart;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,15 @@
-import Chart from "../components/Chart";
+import BarChart from "../components/BarChart";
 import { Col, ListGroup, Row } from "react-bootstrap";
 
 const Home = () => {
   return (
     <Row>
       <Col xs={12} md={8}>
-        <h1>Welcome to VisualFin</h1>
-        <Chart />
+        <h1>Primary Feasibility</h1>
+
+        {/* ---------------------------------------Bar Chart--------------------------------------- */}
+        <BarChart />
+
         <ListGroup style={{ width: "100%" }} horizontal={"sm"}>
           <ListGroup.Item>
             <span className="text-secondary">Total revenue</span> <br />
@@ -27,17 +30,18 @@ const Home = () => {
           </ListGroup.Item>
         </ListGroup>
 
-        {/* Temporary image. Later it will be replaced by map */}
-        <div
+        {/* ---------------------------------------Google Map--------------------------------------- */}
+        <iframe
           className="my-3"
-          style={{
-            height: "20rem",
-            overflow: "hidden",
-          }}
-        >
-          <img src="https://cdn.hswstatic.com/gif/maps.jpg" alt="Map" />
-        </div>
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238133.1528393404!2d72.68186492252839!3d21.15914221757627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e59411d1563%3A0xfe4558290938b042!2sSurat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1646369466554!5m2!1sen!2sin"
+          width="100%"
+          height="250"
+          allowfullscreen=""
+          loading="lazy"
+          title="google map"
+        ></iframe>
       </Col>
+
       <Col xs={12} md={4}>
         Project Calculator
       </Col>
